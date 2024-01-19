a95fc07c5d9c439c15950138370fe4ce2d49f6af
Esperanza211 <khadijazaabar05@gmail.com>
2024-01-19 13:14:31 -0800

    configuation

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
-#!/usr/bin/pup
-# install an especific version of flask (2.1.0)
+#!/usr/bin/puppet
+# Install Flask and Werkzeug
 package { 'flask':
-    ensure   => '2.1.0',
-    provider => 'pip3',
+  ensure   => '2.1.0',
+  provider => 'pip3',
+}
+
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
 }
